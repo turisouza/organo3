b4c2c0e6b1a3fb7af978fd09062c1a85747570bd
Turi Souza <turisouza@MacBook-Pro-de-Turi.local>
2024-02-15 22:20:23 -0300

    “mensagem”Upload2

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,40 @@
-import './colaborador.css'
+import { IoIosCloseCircle } from "react-icons/io";
+import "./colaborador.css";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+  function favoritar() {
+    aoFavoritar(colaborador.id);
+  }
 
-export default Colaborador
+  const propsFavorito = {
+    size: 25,
+    onClick: favoritar,
+  };
+
+  return (
+    <div className="colaborador">
+      <IoIosCloseCircle
+        size={25}
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito ? (
+            <MdFavorite {...propsFavorito} color="#ff0000" />
+          ) : (
+            <MdFavoriteBorder {...propsFavorito} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
