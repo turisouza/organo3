c72fa7675bce1e04dd7d1c0c83ed3bb33cc78785
Turi Souza <turisouza@MacBook-Pro-de-Turi.local>
2024-02-21 20:07:34 -0300

    Inclusão dos Bancos de Dados JSON para os Times e os Colaboradores

--- src/App.js
+++ src/App.js
@@ -9,43 +9,15 @@ import { MdPreview } from "react-icons/md";
 import "./App.css";
 
 function App() {
-  const [times, setTimes] = useState([
-    {
-      id: uuidv4(),
-      nome: "Programação",
-      cor: "#57C278",
-    },
-    {
-      id: uuidv4(),
-      nome: "Front-End",
-      cor: "#82CFFA",
-    },
-    {
-      id: uuidv4(),
-      nome: "Data Science",
-      cor: "#A6D157",
-    },
-    {
-      id: uuidv4(),
-      nome: "Devops",
-      cor: "#E06B69",
-    },
-    {
-      id: uuidv4(),
-      nome: "UX e Design",
-      cor: "#DB6EBF",
-    },
-    {
-      id: uuidv4(),
-      nome: "Mobile",
-      cor: "#FFBA05",
-    },
-    {
-      id: uuidv4(),
-      nome: "Inovação e Gestão",
-      cor: "#FF8A29",
-    },
-  ]);
+  const [times, setTimes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7070/times")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setTimes(dados);
+      });
+  }, []);
 
   const [colaboradores, setColaboradores] = useState([]);
 
